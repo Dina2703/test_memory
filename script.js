4dcf77f86a6ebbcbf90b69cac98b2bd9ad4dd5de
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-09-27 20:50:27 -0400

    Flip Card

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const cards = document.querySelectorAll(".card");
 
-const flipFunc = (el) => {
-  el.classList.toggle("flip");
-};
+function flipCard() {
+  this.classList.toggle("flip");
+}
 
-cards.forEach((card, index) =>
-  card.addEventListener("click", () => console.log(index + " clicked"))
-);
-// console.log(cards);
+cards.forEach((card) => card.addEventListener("click", flipCard));
